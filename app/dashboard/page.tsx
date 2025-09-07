7d9f957d6d4195cff16825493c3a075d629a20de
john lester escarlan <jlescarlan11@gmail.com>
2025-09-07 11:03:59 +0800

    fixed api

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -51,7 +51,7 @@ interface UrlData {
 }
 
 // Constants
-const API_BASE_URL = "http://localhost:8080/api/urls";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 const DashboardPage = () => {
   // Core state
